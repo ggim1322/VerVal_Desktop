e1e3eb9b15049ad32ed77330d7cd7aab8aeaf72d
Gázsa Gergő <gazsa.gergo.dev@gmail.com>
2016-04-03 01:32:32 +0300

    Lab 2: Testcases were added - simple test, exception test, parameterized test.

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ A LogAnalyser class was created with an isValidLogFileName function, which:
 * checks if a log file name is valid.
 * if the logfile name's length is shorter than ".str"'s length, the method throws an exception.
 
+## Lab 2: Junit testing.
+
+Testcases were created to check the functionality of the LogAnalyser class's isValidLogFileName method:
+* simple test.
+* test for exceptions.
+* parameterized test.
---
+++ src/test/java/loganalysertest/LogAnalyserParameterizedTest.java
@@ -0,0 +1,59 @@
+package loganalysertest;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import loganalyser.LogAnalyser;
+
+
+/**
+ * Created by Gazsa Gergo on 2/29/16
+ */
+@RunWith(Parameterized.class)
+public class LogAnalyserParameterizedTest {
+
+    public String fileName;
+    public boolean expectedReturn;
+	
+	public LogAnalyserParameterizedTest(String fileName, boolean expectedReturn) {
+		this.fileName = fileName;
+		this.expectedReturn = expectedReturn;
+	}
+	
+    @Parameters
+    public static Collection<Object[]> testData() {
+	    Object[][] data = new Object[][] {
+	        { "testFileName.str1", false }, 
+	        { "testFileName.str", true }, 
+	        { "testFileName.str2", false }
+	    };
+	    return Arrays.asList(data);
+    }
+    
+    private LogAnalyser logA;
+
+    @Before
+    public void setup() throws Exception {
+        logA = new LogAnalyser();
+    }
+
+    @After
+    public void tearDown() throws Exception {}
+    
+    
+    @Test
+    public void isValidLogFileName_ValidName_ReturnsTrue() throws Exception {
+        assertEquals(expectedReturn, logA.isValidLogFileName(fileName));
+    }
+    
+    
+}
